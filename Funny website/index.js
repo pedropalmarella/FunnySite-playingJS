f6526879578eaaa5bff8a15c90a13d3b28d74ff4
Pedro Henrique Palmarella Rodrigues <109292728+pedropalmarella@users.noreply.github.com>
2024-03-07 01:18:24 -0300

    Add files via upload

---
+++ Funny website/index.js
@@ -0,0 +1,9 @@
+function start() {
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var language = document.getElementById("language").value;
+    alert(
+        "Hello, how are you " + name + "? You have " + age + " and are studying " 
+        + language + " wow, I hope you have a lot sucess in your studies!"
+    );
+}
